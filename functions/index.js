8ce50035813924d40ce765ccd2cb18b6744388f4
Uddekudde <33427362+Uddekudde@users.noreply.github.com>
2020-02-17 02:00:46 +0100

    install express, get offers

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,31 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+admin.initializeApp();
+
+const express = require("express");
+const app = express();
+const db = admin.firestore();
+
+const OFFER_URL = "/offers";
+
+app.get(OFFER_URL, (req, res) => {
+  db.collection("commission-offers")
+    .get()
+    .then(data => {
+      let offers = [];
+      data.forEach(doc => {
+        offers.push(doc.data());
+      });
+      return res.json(offers);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+exports.api = functions.https.onRequest(app);
