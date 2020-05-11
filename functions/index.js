18c6e05c3845fdab7ae927597cf0c0fe7519c9c6
Uddekudde <33427362+Uddekudde@users.noreply.github.com>
2020-05-11 12:44:49 +0200

    added CORS middleware

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 const {
   getAllOffers,
   postAnOffer,
